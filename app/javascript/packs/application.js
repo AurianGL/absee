416b220593aa949c7b5373153b861d1971839661
antoine schmitt <antoine.schmitt@gmail.com>
2019-08-29 16:22:47 +0200

    radial menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,24 @@
 import "bootstrap";
 import {initDraggable} from "../components/draggable";
 import {initCanvas} from "../components/canvas";
-import {initCountMenuItems} from "../components/radial_menu";
-import {initSetupLinks} from "../components/radial_menu";
-import {initSetupLinkHovers} from "../components/radial_menu";
-import {initOnMenuItemsDropdownChange} from "../components/radial_menu";
-import {initUpdateMenuItemDisplayValues} from "../components/radial_menu";
-import {initMouseMoveListener} from "../components/radial_menu";
-import {initPositionIcons} from "../components/radial_menu";
+// import {initCountMenuItems} from "../components/radial_menu";
+// import {initSetupLinks} from "../components/radial_menu";
+// import {initSetupLinkHovers} from "../components/radial_menu";
+// import {initOnMenuItemsDropdownChange} from "../components/radial_menu";
+// import {initUpdateMenuItemDisplayValues} from "../components/radial_menu";
+// import {initMouseMoveListener} from "../components/radial_menu";
+import radialMenu from "../components/radial_menu";
 
 
 
 
 initDraggable();
 initCanvas();
-initCountMenuItems();
-initSetupLinks();
-initSetupLinkHovers();
-initOnMenuItemsDropdownChange();
-initUpdateMenuItemDisplayValues();
-initMouseMoveListener();
-initPositionIcons();
+// initCountMenuItems();
+// initSetupLinks();
+// initSetupLinkHovers();
+// initOnMenuItemsDropdownChange();
+// initUpdateMenuItemDisplayValues();
+// initMouseMoveListener();
+// initPositionIcons();
+radialMenu();
