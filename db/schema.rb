ed09a63c895d90a99246fc101066d69fc7d5df79
Alice Deveaux <alice.dvx@icloud.com>
2019-09-03 11:38:41 +0200

    finished buttons components

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_122952) do
+ActiveRecord::Schema.define(version: 2019_08_30_103932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2019_08_29_122952) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
+    t.string "portfolio"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
