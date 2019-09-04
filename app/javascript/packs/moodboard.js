442a28ef665fb1070e13d4ef13657667b083d75e
Alice Deveaux <alice.dvx@icloud.com>
2019-09-04 20:08:41 +0200

    front-structure of versions#index

--- app/javascript/packs/moodboard.js
+++ app/javascript/packs/moodboard.js
@@ -1,4 +1,4 @@
-import "bootstrap";
+// import "bootstrap";
 import {initDraggable} from "../components/draggable";
 import {initCanvas} from "../components/canvas";
 import {initToggleVisibility} from "../components/toggle_visibility";
@@ -6,8 +6,6 @@ import {initResize} from "../components/resize"
 import {closeModale} from "../components/closemodale"
 import {initSetZ} from "../components/z_index"
 
-// TWEAK TO MAKE JQUERY AVAILABLE IN RAILSJS ERB
-window.jquery = $;
 
 initSetZ();
 initDraggable();
