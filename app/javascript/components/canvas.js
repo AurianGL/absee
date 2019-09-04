67b3a4837dc3f065f974de0755ba770acbaea04d
Aurian Guerard des Lauriers <aurian@hotmail.fr>
2019-09-04 16:04:02 +0200

    started stuff with canvas

--- app/javascript/components/canvas.js
+++ app/javascript/components/canvas.js
@@ -6,8 +6,8 @@ let initX;
 let context
 
 const start = (event) => {
-  console.log(event);
   currentDrawable = event.currentTarget;
+  console.log(currentDrawable);
   initY = event.offsetY;
   initX = event.offsetX;
   context = currentDrawable.getContext('2d');
@@ -15,13 +15,13 @@ const start = (event) => {
 }
 
 const stop = (event) => {
-  console.log(event);
+  // console.log(event);
   currentDrawable.removeEventListener("mousemove", draw);
 }
 
 const draw = (event) => {
-  console.log(currentDrawable);
-  console.log(event.offsetX, event.offsetY);
+  // console.log(currentDrawable);
+  // console.log(event.offsetX, event.offsetY);
 
   context.lineCap = 'round';
   context.lineJoin = 'round';
---
+++ app/javascript/components/set_size_canvas.js
@@ -0,0 +1,7 @@
+const draggables = document.querySelectorAll(".draggable");
+
+const initSetSize = () => {
+  draggables.forEach((draggable) => {
+    draggable.querySelector("img", findSize)
+  })
+}
